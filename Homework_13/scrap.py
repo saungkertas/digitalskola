04561e5d16734e4439628044caf9d122d5da6fa3
awisbay <64637933+awisbay@users.noreply.github.com>
2022-02-15 04:46:21 -0800

    update HW13

--- Homework_13/scrap.py
+++ Homework_13/scrap.py
@@ -1,14 +1,24 @@
-import json
+import urllib.request, json, csv, timeit, pandas as pd
 
-dir = "C:\\Users\\Default.Default-THINK\\Dropbox\\My PC (Default-THINK)\\Documents\\DigitalSkola\\HW_13\\"
-# Opening JSON file
-#f = open(r'C:\Users\Default.Default-THINK\Dropbox\My PC (Default-THINK)\Documents\DigitalSkola\HW_13\shopee.json')
- 
+dir = "C:\\Users\\Default.Default-THINK\\Dropbox\\My PC (Default-THINK)\\Documents\\GitHub\\digitalskola\\Homework_13\\"
 
-with open(dir + 'shopee.json') as f:
-   data = json.loads(f.read().decode())
-# returns JSON object as
-# a dictionary
-#data = json.loads(f.read().decode())
+shopee_url = "https://shopee.co.id/api/v4/search/search_items?by=relevancy&keyword=kipas%20angin&limit=60&newest=0&order=desc&page_type=search&scenario=PAGE_GLOBAL_SEARCH&version=2"
+print(shopee_url)
+with urllib.request.urlopen(shopee_url) as url:
+     data_shopee = json.loads(url.read().decode('ascii', 'ignore'))
 
-print(data)
+#type(data_shopee)
+#print(data_shopee['items'][0]['item_basic']['name'])
+
+header = ['Product Name','Price'] #
+with open(dir + 'Data_kipas_angin_Shopee_Wisnu.csv','a') as file: # write new csv file
+   writer = csv.writer(file, lineterminator='\n') # assign variable for csv writer with remove newline
+   writer.writerow(header) 
+   items = data_shopee['items']
+
+   for item in items:
+      name = item['item_basic']['name']
+      price = item['item_basic']['price']
+      data_price = [name, int(price/100000)]
+      #print(data_price)
+      writer.writerow(data_price)
